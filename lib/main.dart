ab9a13c4227e605064e60ca0e03b32bde2cb9610
Eliezer Cútalo <eliezercsj@gmail.com>
2019-08-05 11:48:02 -0300

    Iniciando tela de login

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:gerente_loja/screens/login_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Gerente de loja',
+      theme: ThemeData(
+        primaryColor: Colors.pinkAccent
+      ),
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
